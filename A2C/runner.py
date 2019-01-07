db2a22b58b9e98dff8eaf426d09d83a13c9811d9
Unknown <oliver.yates01@gmail.com>
2019-01-07 11:25:32 +0100

    rectified sampling rate for nn training

--- A2C/runner.py
+++ A2C/runner.py
@@ -120,5 +120,5 @@ with sc2_env.SC2Env(agent_race=None,
                       .format(e, MAX_EPISODES, score))
                 agent.train()
                 break
-            agent.train()
+        agent.train()
         agent.save("./save/move_2_beacon-dqn.h5")
